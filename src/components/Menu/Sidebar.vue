b59ba89cc6ebc56ac68964e5966bb4a74bd29786
David Munkacsi <david.munkacsi93@gmail.com>
2019-10-18 12:00:34 +0200

    Create new sidebar component.

---
+++ src/components/Menu/Sidebar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div>
+      <div @click="closeSidebarPanel" v-if="isPanelOpen"></div>
+    </div>
+    <transition name="slide">
+      <div v-if="isPanelOpen">
+        <slot></slot>
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
