289839b3f870a913ac0b5a4f1a8b677372af0f11
David Munkacsi <david.munkacsi93@gmail.com>
2020-01-26 17:21:42 +0100

    Integrate vue-grid-layout.

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,28 @@
         </li>
       </ul>
     </sidebar>
-    <grid-layout :layoutItems.sync="layoutItems">
+    <grid-layout
+      :layout.sync="layout"
+      :col-num="12"
+      :row-height="30"
+      :is-draggable="true"
+      :is-resizable="true"
+      :is-mirrored="false"
+      :vertical-compact="true"
+      :margin="[10, 10]"
+      :use-css-transforms="true"
+    >
       <grid-item
-        v-for="item in layoutItems"
-        :key="item.id"
-        :id="item.id"
+        v-for="item in layout"
         :x="item.x"
         :y="item.y"
-        :width="item.width"
-        :height="item.height"
-        :isDraggable="item.isDraggable"
-        :isResizable="item.isResizable"
-      ></grid-item>
+        :w="item.w"
+        :h="item.h"
+        :i="item.i"
+        :key="item.i"
+      >
+        {{ item.i }}
+      </grid-item>
     </grid-layout>
   </div>
 </template>
@@ -37,18 +47,45 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MenuBar from "./components/Menu/MenuBar.vue";
 import Sidebar from "./components/Menu/Sidebar.vue";
 
-import GridItem from "./components/Grid/GridItem.vue";
-import GridLayout from "./components/Grid/GridLayout.vue";
+import VueGridLayout from "vue-grid-layout";
 
 import { mapState } from "vuex";
+
+var testLayout = [
+  { x: 0, y: 0, w: 2, h: 2, i: "0" },
+  { x: 2, y: 0, w: 2, h: 4, i: "1" },
+  { x: 4, y: 0, w: 2, h: 5, i: "2" },
+  { x: 6, y: 0, w: 2, h: 3, i: "3" },
+  { x: 8, y: 0, w: 2, h: 3, i: "4" },
+  { x: 10, y: 0, w: 2, h: 3, i: "5" },
+  { x: 0, y: 5, w: 2, h: 5, i: "6" },
+  { x: 2, y: 5, w: 2, h: 5, i: "7" },
+  { x: 4, y: 5, w: 2, h: 5, i: "8" },
+  { x: 6, y: 3, w: 2, h: 4, i: "9" },
+  { x: 8, y: 4, w: 2, h: 4, i: "10" },
+  { x: 10, y: 4, w: 2, h: 4, i: "11" },
+  { x: 0, y: 10, w: 2, h: 5, i: "12" },
+  { x: 2, y: 10, w: 2, h: 5, i: "13" },
+  { x: 4, y: 8, w: 2, h: 4, i: "14" },
+  { x: 6, y: 8, w: 2, h: 4, i: "15" },
+  { x: 8, y: 10, w: 2, h: 5, i: "16" },
+  { x: 10, y: 4, w: 2, h: 2, i: "17" },
+  { x: 0, y: 9, w: 2, h: 3, i: "18" },
+  { x: 2, y: 6, w: 2, h: 2, i: "19" }
+];
 
 export default {
   name: "app",
   components: {
     MenuBar,
     Sidebar,
-    GridItem,
-    GridLayout
+    GridItem: VueGridLayout.GridItem,
+    GridLayout: VueGridLayout.GridLayout
+  },
+  data() {
+    return {
+      layout: testLayout
+    };
   },
   computed: {
     ...mapState({
