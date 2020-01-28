891eaaa4627fb228cf420c0f6c00cbd739572777
David Munkacsi <david.munkacsi93@gmail.com>
2020-01-28 13:35:51 +0100

    Remove unused method.

--- src/components/GridLayout.vue
+++ src/components/GridLayout.vue
@@ -6,17 +6,12 @@ export default {
   name: "GridLayout",
   extends: GridLayout,
   created: function() {
-    EventBus.$on("gridItemAdded", this.handleGridItemAdded);
     EventBus.$on("layoutUpdated", this.handleLayoutUpdated);
   },
   beforeDestroy: function() {
-    EventBus.$off("gridItemAdded", this.handleGridItemAdded);
     EventBus.$off("layoutUpdated", this.handleLayoutUpdated);
   },
   methods: {
-    handleGridItemAdded() {
-      this.layoutUpdate();
-    },
     handleLayoutUpdated() {
       console.log(this.$data);
       this.layoutUpdate();
