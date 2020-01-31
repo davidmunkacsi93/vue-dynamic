e9fafabc7e8a0e44c74a96a07dc4554030498890
David Munkacsi <david.munkacsi93@gmail.com>
2020-01-31 13:10:05 +0100

    Rollback grid-layout within menu bar.

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,90 +1,29 @@
 <template>
-  <grid-layout
-    :layout.sync="menuLayoutItems"
-    :col-num="24"
-    :row-height="30"
-    :margin="[0, 0]"
-    :is-draggable="true"
-    :is-resizable="true"
-    :is-mirrored="false"
-    :responsive="true"
-    :vertical-compact="true"
-    :use-css-transforms="true"
-  >
-    <grid-item
-      :x="0"
-      :y="0"
-      :w="1"
-      :h="1"
-      :i="100"
-      :isDraggable="true"
-      :isResizable="false"
-      :static="false"
-    >
-      <button
-        type="button"
-        class="menu-item fa fa-bars fa-3x"
-        title="Menu"
-        @click.prevent="toggle"
-      ></button>
-    </grid-item>
-    <grid-item
-      :x="0"
-      :y="0"
-      :w="1"
-      :h="1"
-      :i="100"
-      :isDraggable="true"
-      :isResizable="false"
-      :static="false"
-    >
-      <button
-        type="button"
-        class="menu-item fa fa-bars fa-3x"
-        title="Menu"
-        @click.prevent="toggle"
-      ></button>
-    </grid-item>
-    <grid-item
-      :x="1"
-      :y="0"
-      :w="1"
-      :h="1"
-      :i="100"
-      :isDraggable="true"
-      :isResizable="false"
-      :static="false"
-    >
-      <button
-        type="button"
-        class="menu-item fa fa-plus fa-3x"
-        @click.prevent="addGridItem"
-      ></button>
-    </grid-item>
-    <grid-item
-      :x="2"
-      :y="0"
-      :w="1"
-      :h="1"
-      :i="100"
-      :isDraggable="true"
-      :isResizable="false"
-      :static="false"
-    >
-      <button
-        v-if="!isEditModeEnabled"
-        type="button"
-        class="menu-item fa fa-pencil fa-3x"
-        @click.prevent="editGridLayout"
-      ></button>
-      <button
-        v-if="isEditModeEnabled"
-        type="button"
-        class="menu-item fa fa-floppy-o fa-3x"
-        @click.prevent="saveLayout"
-      ></button>
-    </grid-item>
-  </grid-layout>
+  <div>
+    <button
+      type="button"
+      class="menu-item fa fa-bars fa-3x"
+      title="Menu"
+      @click.prevent="toggle"
+    ></button>
+    <button
+      type="button"
+      class="menu-item fa fa-plus fa-3x"
+      @click.prevent="addGridItem"
+    ></button>
+    <button
+      v-if="!isEditModeEnabled"
+      type="button"
+      class="menu-item fa fa-pencil fa-3x"
+      @click.prevent="editGridLayout"
+    ></button>
+    <button
+      v-if="isEditModeEnabled"
+      type="button"
+      class="menu-item fa fa-floppy-o fa-3x"
+      @click.prevent="saveLayout"
+    ></button>
+  </div>
 </template>
 
 <script>
