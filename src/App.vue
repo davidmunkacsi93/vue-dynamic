8dd17ba71553aef6e434047f06cc3bd7438909e7
David Munkacsi <david.munkacsi93@gmail.com>
2020-02-06 12:07:37 +0100

    Use default material style sheet.

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@
 import Vue from "vue";
 import VueMaterial from "vue-material";
 import "vue-material/dist/vue-material.min.css";
-import "vue-material/dist/theme/default-dark.css";
+import "vue-material/dist/theme/default.css";
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
