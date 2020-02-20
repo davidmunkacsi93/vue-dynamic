8b5d6fb2812ba27357f2f081473a0ae86125268f
David Munkacsi <david.munkacsi93@gmail.com>
2020-02-20 15:02:38 +0100

    Implement first approach of the API intergration service.

--- src/services/api-integration-service.js
+++ src/services/api-integration-service.js
@@ -1,1 +1,13 @@
-export default class ApiIntegrationService {}
+import axios from "axios";
+import SwaggerParser from "swagger-parser";
+
+export default class ApiIntegrationService {
+  getSwaggerSpecification(url) {
+    var swaggerSpecification = axios
+      .get(url)
+      .then(response => console.log(response));
+
+    var parsedSpecification = SwaggerParser.parse(swaggerSpecification);
+    console.log(parsedSpecification)
+  }
+}
