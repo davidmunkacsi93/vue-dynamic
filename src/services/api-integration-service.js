7541387a247eab633ee296d604ccd9064c77368b
David Munkacsi <david.munkacsi93@gmail.com>
2020-02-21 13:12:48 +0100

    Implement API integration service WIP.

--- src/services/api-integration-service.js
+++ src/services/api-integration-service.js
@@ -2,12 +2,40 @@ import axios from "axios";
 import SwaggerParser from "swagger-parser";
 
 class ApiIntegrationService {
-  getSwaggerSpecification(url) {
-    var swaggerSpecification = axios
+  async getSwaggerSpecification(url) {
+    axios
       .get(url)
-      .then(response => console.log(response));
+      .then(response => {
+        SwaggerParser.validate(response.data)
+          .then(async () => {
+            let parsedSpecification;
+            if (response.headers["content-type"] === "application/yaml") {
+              parsedSpecification = await SwaggerParser.parse(response.data);
+            } else if (response.headers["content-type"] == "application/json") {
+              parsedSpecification = response.data;
+            } else {
+              console.error(
+                `Specification format ${
+                  response.headers["content-type"]
+                } is not supported.`
+              );
+            }
+            console.log(parsedSpecification);
+          })
+          .catch(() => {
+            console.log("Invalid specification");
+          });
+      })
+      .catch(response => {
+        console.error(response.code);
+      });
+  }
 
-    return SwaggerParser.parse(swaggerSpecification);
+  parseSpecification() {
+    const specification = require("../example-spec/las-swagger.json");
+    SwaggerParser.parse(specification).then(apiDocumentation =>
+      console.log(apiDocumentation)
+    );
   }
 }
 
