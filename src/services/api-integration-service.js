ae5e19c6c2fe5bbaa35cbe636aad5c80aad88b9d
David Munkacsi <david.munkacsi93@gmail.com>
2020-02-21 13:39:34 +0100

    Implement base process of API integration service.

--- src/services/api-integration-service.js
+++ src/services/api-integration-service.js
@@ -1,41 +1,29 @@
-import axios from "axios";
 import SwaggerParser from "swagger-parser";
 
 class ApiIntegrationService {
   async getSwaggerSpecification(url) {
-    axios
-      .get(url)
-      .then(response => {
-        SwaggerParser.validate(response.data)
-          .then(async () => {
-            let parsedSpecification;
-            if (response.headers["content-type"] === "application/yaml") {
-              parsedSpecification = await SwaggerParser.parse(response.data);
-            } else if (response.headers["content-type"] == "application/json") {
-              parsedSpecification = response.data;
-            } else {
-              console.error(
-                `Specification format ${
-                  response.headers["content-type"]
-                } is not supported.`
-              );
-            }
-            console.log(parsedSpecification);
-          })
-          .catch(() => {
-            console.log("Invalid specification");
-          });
+    SwaggerParser.validate(url)
+      .then(async () => {
+        let parsedSpecification = await SwaggerParser.parse(url);
+        if (parsedSpecification.swagger === "2.0") {
+          this.processOpenApi2(parsedSpecification);
+        } else if (parsedSpecification.openapi === "3.0.0") {
+          this.processOpenApi3(parsedSpecification);
+        } else {
+          console.error("Unknown specification or version detected.");
+        }
       })
-      .catch(response => {
-        console.error(response.code);
+      .catch(reason => {
+        console.log(reason);
       });
   }
 
-  parseSpecification() {
-    const specification = require("../example-spec/las-swagger.json");
-    SwaggerParser.parse(specification).then(apiDocumentation =>
-      console.log(apiDocumentation)
-    );
+  processOpenApi2(parsedSpecification) {
+    console.log("Parsing swagger specificaiton version 2.0");
+  }
+
+  processOpenApi3(parsedSpecification) {
+    console.log("Parsing swagger specificaiton version 3.0.0");
   }
 }
 
