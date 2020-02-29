527dd5288c0091b42dfad805d05ff6ad809a2f0e
David Munkacsi <david.munkacsi93@gmail.com>
2020-02-29 17:29:09 +0100

    Create layout for every screen size class.

--- src/store/modules/main-layout.js
+++ src/store/modules/main-layout.js
@@ -11,7 +11,103 @@ import { CONTENT, MENU, NAVIGATION_BAR } from "../../types/layout-item-types";
 import { COMPACT, LAYOUT_UPDATED } from "../../types/event-types";
 import { v1 as uuid } from "uuid";
 
-const LOCAL_STORAGE_MAIN_LAYOUT_KEY = "main-layout";
+function getLayoutItemsForLg() {
+  var layoutItems = [];
+  const menu = {
+    x: 2,
+    y: 0,
+    w: 10,
+    h: 2,
+    i: uuid(),
+    isDraggable: true,
+    isResizable: true,
+    static: true,
+    layoutItemType: MENU
+  };
+  const content = {
+    x: 2,
+    y: 2,
+    w: 10,
+    h: 10,
+    i: uuid(),
+    isDraggable: true,
+    isResizable: true,
+    static: true,
+    layoutItemType: CONTENT
+  };
+  const navigationBar = {
+    x: 0,
+    y: 0,
+    w: 2,
+    h: 15,
+    i: uuid(),
+    isDraggable: true,
+    isResizable: true,
+    static: true,
+    layoutItemType: NAVIGATION_BAR
+  };
+  layoutItems.push(menu);
+  layoutItems.push(navigationBar);
+  layoutItems.push(content);
+
+  return layoutItems;
+}
+
+function getLayoutItemsForMd() {
+  return getLayoutItemsForLg();
+}
+
+function getLayoutItemsForSm() {
+  var layoutItems = [];
+  const menu = {
+    x: 2,
+    y: 0,
+    w: 10,
+    h: 2,
+    i: uuid(),
+    isDraggable: true,
+    isResizable: true,
+    static: true,
+    layoutItemType: MENU
+  };
+  const content = {
+    x: 2,
+    y: 2,
+    w: 10,
+    h: 10,
+    i: uuid(),
+    isDraggable: true,
+    isResizable: true,
+    static: true,
+    layoutItemType: CONTENT
+  };
+  const navigationBar = {
+    x: 0,
+    y: 0,
+    w: 2,
+    h: 15,
+    i: uuid(),
+    isDraggable: true,
+    isResizable: true,
+    static: true,
+    layoutItemType: NAVIGATION_BAR
+  };
+  layoutItems.push(menu);
+  layoutItems.push(navigationBar);
+  layoutItems.push(content);
+
+  return layoutItems;
+}
+
+function getLayoutItemsForXs() {
+  return getLayoutItemsForSm();
+}
+
+function getLayoutItemsForXxs() {
+  return getLayoutItemsForXs();
+}
+
+const LOCAL_STORAGE_MAIN_LAYOUT_KEY = "main-layouts";
 
 const state = {
   isEditModeActive: false,
@@ -36,47 +132,9 @@ const mutations = {
     const layoutString = localStorage.getItem(LOCAL_STORAGE_MAIN_LAYOUT_KEY);
     if (layoutString) return;
 
-    const menu = {
-      x: 2,
-      y: 0,
-      w: 10,
-      h: 2,
-      i: uuid(),
-      isDraggable: true,
-      isResizable: true,
-      static: true,
-      layoutItemType: MENU
-    };
-    const content = {
-      x: 2,
-      y: 2,
-      w: 10,
-      h: 10,
-      i: uuid(),
-      isDraggable: true,
-      isResizable: true,
-      static: true,
-      layoutItemType: CONTENT
-    };
-    const navigationBar = {
-      x: 0,
-      y: 0,
-      w: 2,
-      h: 15,
-      i: uuid(),
-      isDraggable: true,
-      isResizable: true,
-      static: true,
-      layoutItemType: NAVIGATION_BAR
-    };
-
-    state.mainLayoutItems.push(menu);
-    state.mainLayoutItems.push(content);
-    state.mainLayoutItems.push(navigationBar);
-
     localStorage.setItem(
       LOCAL_STORAGE_MAIN_LAYOUT_KEY,
-      JSON.stringify(state.mainLayoutItems)
+      JSON.stringify(state.mainLayouts)
     );
   },
   loadMainLayout(state) {
