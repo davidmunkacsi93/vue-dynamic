167340fcea3fba86a69c0387e778310351c33945
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-05 14:28:08 +0100

    Move component creation logic to factory.

--- src/parsers/open-api-2-parser.js
+++ src/parsers/open-api-2-parser.js
@@ -1,5 +1,5 @@
-import { FORM, INPUT, DROP_DOWN, SWITCH } from "../types/layout-item-types";
 import ApiModelFactory from "../factories/api-model-factory";
+import DynamicComponentFactory from "../factories/dynamic-component-factory";
 import OpenApiInformationProvider from "../providers/open-api-information-provider";
 
 class OpenApi2Parser {
@@ -17,7 +17,7 @@ class OpenApi2Parser {
       specification.definitions,
       "#/definitions/"
     );
-    var dynamicComponents = this.createDynamicComponentsForApi(
+    var dynamicComponents = DynamicComponentFactory.createDynamicComponentsForApi(
       specification.paths,
       apiModels.apiModels
     );
@@ -32,107 +32,6 @@ class OpenApi2Parser {
     };
 
     return apiUIModel;
-  }
-
-  createDynamicComponentsForApi(apiEndpoints, apiModels) {
-    var dynamicComponents = [];
-
-    for (var endpoint in apiEndpoints) {
-      var apiEndpoint = apiEndpoints[endpoint];
-
-      for (var httpMethod in apiEndpoint) {
-        var apiMethod = apiEndpoint[httpMethod];
-        var dynamicComponent = {};
-        dynamicComponent.path = endpoint;
-        dynamicComponent.description = apiMethod.description;
-        dynamicComponent.httpMethod = httpMethod.toUpperCase();
-
-        if (httpMethod === "get") {
-          // Based on the parameters specified generate a search form,
-          // that transform into a grid / list / input based on the response type.
-        } else if (
-          httpMethod === "post" ||
-          httpMethod === "delete" ||
-          httpMethod === "put"
-        ) {
-          dynamicComponent.type = FORM;
-          dynamicComponent.controls = this.createControlsForDynamicForm(
-            apiMethod,
-            apiModels
-          );
-        } else {
-          console.log(httpMethod);
-          console.error("Not supported HTTP method.");
-        }
-
-        dynamicComponents.push(dynamicComponent);
-      }
-    }
-    return dynamicComponents;
-  }
-
-  createControlsForDynamicForm(apiMethod, apiModels) {
-    var controls = [];
-    if (apiMethod.parameters) {
-      controls = this.createControlsForParameters(apiMethod, apiModels);
-    } else if (apiMethod.requestBody) {
-      controls = this.createControlsForSchema(apiMethod, apiModels);
-    } else {
-      console.error(
-        `Can't generate dynamic component for endpoint ${apiMethod.description}`
-      );
-    }
-    return controls;
-  }
-
-  createControlsForSchema(apiMethod, apiModels) {
-    var controls = [];
-    var schema = apiMethod.requestBody.content["application/json"].schema;
-    var apiModelKey = schema.$ref.replace("#/components/schemas/", "");
-    var apiModelForSchema = apiModels.find(model => model.type === apiModelKey);
-    for (var property of apiModelForSchema.properties) {
-      var control = {
-        label: property.name,
-        element: INPUT,
-        in: property.in,
-        type: property.type,
-        format: property.format,
-        placeholder: property.example,
-        isEnum: false
-      };
-      controls.push(control);
-    }
-    return controls;
-  }
-
-  createControlsForParameters(apiMethod) {
-    var controls = [];
-    for (var parameter of apiMethod.parameters) {
-      var control = {
-        label: parameter.name,
-        element: INPUT,
-        in: parameter.in,
-        type: parameter.schema.type,
-        format: parameter.schema.format,
-        placeholder: parameter.schema.example,
-        value: null
-      };
-
-      if (parameter.schema.enum) {
-        if (
-          parameter.schema.enum.every(
-            value => value === true || value === false
-          )
-        ) {
-          control.element = SWITCH;
-        } else {
-          control.element = DROP_DOWN;
-          control.values = parameter.schema.enum;
-        }
-      }
-      controls.push(control);
-    }
-    return controls;
   }
 }
 
