d5c1e9ea3417dfe1482cd7e7ed0e839b2bc0a8ed
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-14 20:20:31 +0100

    Introduce dynamic component for better reusability.

---
+++ src/components/DynamicComponent.vue
@@ -0,0 +1,94 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import {
+  REMOVE_FORM,
+  SET_API_ITEM_HEIGHT,
+  SET_API_ITEM_WIDTH,
+  SET_API_ITEM_INTIAILIZED
+} from "../types/action-types";
+
+export default {
+  props: {
+    apiName: {
+      type: String,
+      required: false
+    },
+    controls: {
+      type: Array,
+      required: true
+    },
+    description: {
+      type: String,
+      required: false
+    },
+    httpMethod: {
+      type: String,
+      required: true
+    },
+    initialized: {
+      type: Boolean,
+      required: true
+    },
+    path: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  mounted() {
+    console.log(this.initialized);
+    if (this.initialized) {
+      this.setGridItemHeight();
+      this.setGridItemWidth();
+      this.$store.dispatch(SET_API_ITEM_INTIAILIZED, this.$parent.$attrs.uuid);
+    }
+  },
+  data() {
+    return {
+      editable: true,
+      isDragging: false,
+      delayedDragging: false
+    };
+  },
+  methods: {
+    removeGridItem() {
+      this.$store.dispatch(REMOVE_FORM, this.id);
+    },
+    setGridItemHeight() {
+      var gridItem = this.$parent;
+      var rowHeight = this.$parent.rowHeight;
+      var cardHeight = this.$refs.dynamicForm.$el.clientHeight;
+      gridItem.innerH = Math.ceil(cardHeight / rowHeight);
+      this.$store.dispatch(SET_API_ITEM_HEIGHT, {
+        uuid: gridItem.$attrs.uuid,
+        height: gridItem.innerH
+      });
+    },
+    setGridItemWidth() {
+      var gridItem = this.$parent;
+      var canvas = this.$refs.canvas;
+      var context = canvas.getContext("2d");
+      context.font = "bold 24px Roboto";
+
+      var textMetrics = context.measureText(this.$refs.cardTitle.innerText);
+      var colWidth = gridItem.containerWidth / gridItem.cols;
+      var calculatedWidth = Math.ceil(textMetrics.width / colWidth);
+      if (calculatedWidth > gridItem.innerW) {
+        gridItem.innerW = calculatedWidth;
+        this.$store.dispatch(SET_API_ITEM_WIDTH, {
+          uuid: gridItem.$attrs.uuid,
+          width: gridItem.innerW
+        });
+      }
+    }
+  }
+};
+</script>
+
+<style></style>
