13d4fe46b0b638af3e4dfa8efa043b8916516436
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-15 16:32:36 +0100

    Overload not required properties in dynamicheader.

--- src/components/DynamicHeader.vue
+++ src/components/DynamicHeader.vue
@@ -9,17 +9,35 @@
   </div>
 </template>
 <script>
+import DynamicComponent from "./DynamicComponent.vue";
 export default {
+  extends: DynamicComponent,
   props: {
     apiVersion: {
       type: String,
       required: true
     },
-    description: {
+    controls: {
+      type: Array,
+      required: false
+    },
+    httpMethod: {
+      type: String,
+      required: false
+    },
+    initialized: {
+      type: Boolean,
+      required: false
+    },
+    path: {
       type: String,
       required: false
     },
     title: {
+      type: String,
+      required: true
+    },
+    type: {
       type: String,
       required: true
     }
