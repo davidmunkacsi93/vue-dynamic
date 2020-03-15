3463e0aa12927f9c00e460d47fbdff77b1990204
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-15 16:33:05 +0100

    Remove unnecessary imports.

--- src/components/DynamicForm.vue
+++ src/components/DynamicForm.vue
@@ -67,16 +67,9 @@
     <md-card-actions>
       <md-button>{{ httpMethod }}</md-button>
     </md-card-actions>
-    <canvas ref="canvas" width="0" height="0"></canvas>
   </md-card>
 </template>
 <script>
-import {
-  REMOVE_FORM,
-  SET_API_ITEM_HEIGHT,
-  SET_API_ITEM_WIDTH,
-  SET_API_ITEM_INTIAILIZED
-} from "../types/action-types";
 import {
   DROP_DOWN,
   FLOAT_INPUT,
@@ -86,45 +79,10 @@ import {
   SWITCH,
   DATE_PICKER
 } from "../types/layout-item-types";
+import DynamicComponent from "./DynamicComponent.vue";
 
 export default {
-  props: {
-    apiName: {
-      type: String,
-      required: false
-    },
-    controls: {
-      type: Array,
-      required: true
-    },
-    description: {
-      type: String,
-      required: false
-    },
-    httpMethod: {
-      type: String,
-      required: true
-    },
-    initialized: {
-      type: Boolean,
-      required: true
-    },
-    path: {
-      type: String,
-      required: true
-    },
-    type: {
-      type: String,
-      required: true
-    }
-  },
-  mounted() {
-    if (!this.initialized) {
-      this.setGridItemHeight();
-      this.setGridItemWidth();
-      this.$store.dispatch(SET_API_ITEM_INTIAILIZED, this.$parent.$attrs.uuid);
-    }
-  },
+  extends: DynamicComponent,
   data() {
     return {
       DATE_PICKER: DATE_PICKER,
@@ -133,44 +91,8 @@ export default {
       NUMBER_INPUT: NUMBER_INPUT,
       PASSWORD_INPUT: PASSWORD_INPUT,
       TEXT_INPUT: TEXT_INPUT,
-      SWITCH: SWITCH,
-
-      editable: true,
-      isDragging: false,
-      delayedDragging: false
+      SWITCH: SWITCH
     };
-  },
-  methods: {
-    removeGridItem() {
-      this.$store.dispatch(REMOVE_FORM, this.id);
-    },
-    setGridItemHeight() {
-      var gridItem = this.$parent;
-      var rowHeight = this.$parent.rowHeight;
-      var cardHeight = this.$refs.dynamicForm.$el.clientHeight;
-      gridItem.innerH = Math.ceil(cardHeight / rowHeight);
-      this.$store.dispatch(SET_API_ITEM_HEIGHT, {
-        uuid: gridItem.$attrs.uuid,
-        height: gridItem.innerH
-      });
-    },
-    setGridItemWidth() {
-      var gridItem = this.$parent;
-      var canvas = this.$refs.canvas;
-      var context = canvas.getContext("2d");
-      context.font = "bold 24px Roboto";
-
-      var textMetrics = context.measureText(this.$refs.cardTitle.innerText);
-      var colWidth = gridItem.containerWidth / gridItem.cols;
-      var calculatedWidth = Math.ceil(textMetrics.width / colWidth);
-      if (calculatedWidth > gridItem.innerW) {
-        gridItem.innerW = calculatedWidth;
-        this.$store.dispatch(SET_API_ITEM_WIDTH, {
-          uuid: gridItem.$attrs.uuid,
-          width: gridItem.innerW
-        });
-      }
-    }
   }
 };
 </script>
