51b0e31f5b278057d763e836ae104dd092c64f91
David Munkacsi <davidmunkacsi@Davids-MacBook-Pro.local>
2020-03-17 11:14:40 +0100

    Use bin-packing algorithm to pack initial layout.

--- src/components/GridLayout.vue
+++ src/components/GridLayout.vue
@@ -2,10 +2,17 @@
 import { GridLayout } from "vue-grid-layout";
 import EventBus from "../utils/event-bus.js";
 import { LAYOUT_UPDATED } from "../types/event-types";
+import BinPack from "bin-pack";
 
 export default {
   name: "GridLayout",
   extends: GridLayout,
+  props: {
+    packingRequired: {
+      type: Boolean,
+      required: true
+    }
+  },
   created: function() {
     EventBus.$on(LAYOUT_UPDATED, this.handleLayoutUpdated);
   },
@@ -13,11 +20,34 @@ export default {
     EventBus.$off(LAYOUT_UPDATED, this.handleLayoutUpdated);
   },
   mounted() {
-    setTimeout(() => {}, 300);
+    setTimeout(() => {
+      this.binPackLayoutItems();
+    }, 300);
   },
   methods: {
     handleLayoutUpdated() {
       this.layoutUpdate();
+    },
+    binPackLayoutItems() {
+      if (!this.layout) return;
+      console.log(this.layout);
+      var bins = this.layout.map(layoutItem => {
+        return {
+          height: layoutItem.h,
+          width: layoutItem.w,
+          uuid: layoutItem.uuid
+        };
+      });
+      var binPackedLayout = BinPack(bins);
+
+      if (!binPackedLayout) return;
+
+      binPackedLayout.items.forEach(bin => {
+        var uuid = bin.item.uuid;
+        var layoutItem = this.layout.find(item => item.uuid == uuid);
+        // layoutItem.x = bin.x;
+        // layoutItem.y = bin.y;
+      });
     }
   }
 };
