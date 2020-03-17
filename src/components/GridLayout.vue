30e6e357a451b13611d1ca4648303473e27eb1d4
David Munkacsi <davidmunkacsi@Davids-MacBook-Pro.local>
2020-03-17 12:41:25 +0100

    Remove bin-packing algorithm.

--- src/components/GridLayout.vue
+++ src/components/GridLayout.vue
@@ -2,8 +2,6 @@
 import { GridLayout } from "vue-grid-layout";
 import EventBus from "../utils/event-bus.js";
 import { LAYOUT_UPDATED } from "../types/event-types";
-import BinPack from "bin-pack";
-import { SET_API_LAYOUT_ITEMS } from '../types/action-types';
 
 export default {
   name: "GridLayout",
@@ -22,34 +20,12 @@ export default {
   },
   mounted() {
     setTimeout(() => {
-      if (this.packingRequired) {
-        this.binPackLayoutItems();
-      }
+      this.layoutUpdate();
     }, 300);
   },
   methods: {
     handleLayoutUpdated() {
       this.layoutUpdate();
-    },
-    binPackLayoutItems() {
-      if (!this.layout) return;
-      var bins = this.layout.map(layoutItem => {
-        return {
-          height: layoutItem.h,
-          width: layoutItem.w,
-          uuid: layoutItem.uuid
-        };
-      });
-      var binPackedLayout = BinPack(bins);
-
-      if (!binPackedLayout) return;
-      binPackedLayout.items.forEach(bin => {
-        var uuid = bin.item.uuid;
-        var layoutItem = this.layout.find(item => item.uuid == uuid);
-        layoutItem.x = bin.x % this.colNum;
-        layoutItem.y = bin.y;
-      });
-      this.$store.dispatch(SET_API_LAYOUT_ITEMS, this.layout);
     }
   }
 };
