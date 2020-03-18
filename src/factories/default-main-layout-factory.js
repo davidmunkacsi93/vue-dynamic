72f943c3651113c5c7938742eb32a4f6d0ebe65c
David Munkacsi <davidmunkacsi@Davids-MacBook-Pro.local>
2020-03-18 14:30:27 +0100

    Move factory logic to separate module.

---
+++ src/factories/default-main-layout-factory.js
@@ -0,0 +1,161 @@
+import { v1 as uuid } from "uuid";
+import { CONTENT, MENU, NAVIGATION_BAR } from "../types/layout-item-types";
+
+class DefaultMainLayoutFactory {
+  getDefaultMainLayout() {
+    return {
+      lg: this.getLayoutItemsForLg(),
+      md: this.getLayoutItemsForMd(),
+      sm: this.getLayoutItemsForSm(),
+      xs: this.getLayoutItemsForXs(),
+      xxs: this.getLayoutItemsForXxs()
+    };
+  }
+
+  getLayoutItemsForLg() {
+    var layoutItems = [];
+    const menu = {
+      x: 2,
+      y: 0,
+      w: 10,
+      h: 2,
+      i: uuid(),
+      isDraggable: true,
+      isResizable: true,
+      static: true,
+      initialized: true,
+      type: MENU
+    };
+    const content = {
+      x: 2,
+      y: 2,
+      w: 10,
+      h: 10,
+      i: uuid(),
+      isDraggable: true,
+      isResizable: true,
+      static: true,
+      initialized: true,
+      type: CONTENT
+    };
+    const navigationBar = {
+      x: 0,
+      y: 0,
+      w: 2,
+      h: 15,
+      i: uuid(),
+      isDraggable: true,
+      isResizable: true,
+      static: true,
+      initialized: true,
+      type: NAVIGATION_BAR
+    };
+    layoutItems.push(menu);
+    layoutItems.push(navigationBar);
+    layoutItems.push(content);
+
+    return layoutItems;
+  }
+
+  getLayoutItemsForMd() {
+    var layoutItems = [];
+    const menu = {
+      x: 2,
+      y: 0,
+      w: 8,
+      h: 2,
+      i: uuid(),
+      isDraggable: true,
+      isResizable: true,
+      static: true,
+      initialized: true,
+      type: MENU
+    };
+    const content = {
+      x: 2,
+      y: 2,
+      w: 8,
+      h: 10,
+      i: uuid(),
+      isDraggable: true,
+      isResizable: true,
+      static: true,
+      initialized: true,
+      type: CONTENT
+    };
+    const navigationBar = {
+      x: 0,
+      y: 0,
+      w: 2,
+      h: 15,
+      i: uuid(),
+      isDraggable: true,
+      isResizable: true,
+      static: true,
+      initialized: true,
+      type: NAVIGATION_BAR
+    };
+    layoutItems.push(menu);
+    layoutItems.push(navigationBar);
+    layoutItems.push(content);
+
+    return layoutItems;
+  }
+
+  getLayoutItemsForSm() {
+    var layoutItems = [];
+    const menu = {
+      x: 0,
+      y: 0,
+      w: 6,
+      h: 2,
+      i: uuid(),
+      isDraggable: true,
+      isResizable: true,
+      static: true,
+      initialized: true,
+      type: MENU
+    };
+    const navigationBar = {
+      x: 0,
+      y: 2,
+      w: 6,
+      h: 10,
+      i: uuid(),
+      isDraggable: true,
+      isResizable: true,
+      static: true,
+      initialized: true,
+      type: NAVIGATION_BAR
+    };
+    const content = {
+      x: 0,
+      y: 12,
+      w: 6,
+      h: 10,
+      i: uuid(),
+      isDraggable: true,
+      isResizable: true,
+      static: true,
+      initialized: true,
+      type: CONTENT
+    };
+    layoutItems.push(menu);
+    layoutItems.push(navigationBar);
+    layoutItems.push(content);
+
+    return layoutItems;
+  }
+
+  getLayoutItemsForXs() {
+    return this.getLayoutItemsForSm();
+  }
+
+  getLayoutItemsForXxs() {
+    return this.getLayoutItemsForXs();
+  }
+}
+
+const instance = new DefaultMainLayoutFactory();
+Object.freeze(instance);
+export default instance;
--- src/store/modules/main-layout.js
+++ src/store/modules/main-layout.js
@@ -9,152 +9,9 @@ import {
   SET_NAVIGATION_BAR_HEIGHT,
   SET_SCREEN_CLASS
 } from "../../types/action-types";
-import { CONTENT, MENU, NAVIGATION_BAR } from "../../types/layout-item-types";
+import { CONTENT, NAVIGATION_BAR } from "../../types/layout-item-types";
 import { COMPACT, LAYOUT_UPDATED } from "../../types/event-types";
-import { v1 as uuid } from "uuid";
-
-function getLayoutItemsForLg() {
-  var layoutItems = [];
-  const menu = {
-    x: 2,
-    y: 0,
-    w: 10,
-    h: 2,
-    i: uuid(),
-    isDraggable: true,
-    isResizable: true,
-    static: true,
-    initialized: true,
-    type: MENU
-  };
-  const content = {
-    x: 2,
-    y: 2,
-    w: 10,
-    h: 10,
-    i: uuid(),
-    isDraggable: true,
-    isResizable: true,
-    static: true,
-    initialized: true,
-    type: CONTENT
-  };
-  const navigationBar = {
-    x: 0,
-    y: 0,
-    w: 2,
-    h: 15,
-    i: uuid(),
-    isDraggable: true,
-    isResizable: true,
-    static: true,
-    initialized: true,
-    type: NAVIGATION_BAR
-  };
-  layoutItems.push(menu);
-  layoutItems.push(navigationBar);
-  layoutItems.push(content);
-
-  return layoutItems;
-}
-
-function getLayoutItemsForMd() {
-  var layoutItems = [];
-  const menu = {
-    x: 2,
-    y: 0,
-    w: 8,
-    h: 2,
-    i: uuid(),
-    isDraggable: true,
-    isResizable: true,
-    static: true,
-    initialized: true,
-    type: MENU
-  };
-  const content = {
-    x: 2,
-    y: 2,
-    w: 8,
-    h: 10,
-    i: uuid(),
-    isDraggable: true,
-    isResizable: true,
-    static: true,
-    initialized: true,
-    type: CONTENT
-  };
-  const navigationBar = {
-    x: 0,
-    y: 0,
-    w: 2,
-    h: 15,
-    i: uuid(),
-    isDraggable: true,
-    isResizable: true,
-    static: true,
-    initialized: true,
-    type: NAVIGATION_BAR
-  };
-  layoutItems.push(menu);
-  layoutItems.push(navigationBar);
-  layoutItems.push(content);
-
-  return layoutItems;
-}
-
-function getLayoutItemsForSm() {
-  var layoutItems = [];
-  const menu = {
-    x: 0,
-    y: 0,
-    w: 6,
-    h: 2,
-    i: uuid(),
-    isDraggable: true,
-    isResizable: true,
-    static: true,
-    initialized: true,
-    type: MENU
-  };
-  const navigationBar = {
-    x: 0,
-    y: 2,
-    w: 6,
-    h: 10,
-    i: uuid(),
-    isDraggable: true,
-    isResizable: true,
-    static: true,
-    initialized: true,
-    type: NAVIGATION_BAR
-  };
-  const content = {
-    x: 0,
-    y: 12,
-    w: 6,
-    h: 10,
-    i: uuid(),
-    isDraggable: true,
-    isResizable: true,
-    static: true,
-    initialized: true,
-    type: CONTENT
-  };
-  layoutItems.push(menu);
-  layoutItems.push(navigationBar);
-  layoutItems.push(content);
-
-  return layoutItems;
-}
-
-function getLayoutItemsForXs() {
-  return getLayoutItemsForSm();
-}
-
-function getLayoutItemsForXxs() {
-  return getLayoutItemsForXs();
-}
+import DefaultMainLayoutFactory from "../../factories/default-main-layout-factory";
 
 const LOCAL_STORAGE_MAIN_LAYOUTS_KEY = "main-layouts";
 
@@ -189,13 +46,7 @@ const mutations = {
     const layoutString = localStorage.getItem(LOCAL_STORAGE_MAIN_LAYOUTS_KEY);
     if (layoutString) return;
 
-    state.mainLayouts = {
-      lg: getLayoutItemsForLg(),
-      md: getLayoutItemsForMd(),
-      sm: getLayoutItemsForSm(),
-      xs: getLayoutItemsForXs(),
-      xxs: getLayoutItemsForXxs()
-    };
+    state.mainLayouts = DefaultMainLayoutFactory.getDefaultMainLayout();
 
     state.currentScreenClass = screenClass;
 
