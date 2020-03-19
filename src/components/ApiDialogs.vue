6b99a31a6752ae5839e72dcdc70b4cd140c57c4e
David Munkacsi <davidmunkacsi@Davids-MacBook-Pro.local>
2020-03-19 16:03:25 +0100

    Add api dialogs component.

---
+++ src/components/ApiDialogs.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <md-dialog-alert
+      :md-active.sync="requestSuccessful"
+      md-title="Successful"
+      :md-content="successMessage"
+      md-confirm-text="Ok"
+    />
+    <md-dialog-alert
+      :md-active.sync="requestFailed"
+      md-title="Error"
+      :md-content="errorMessage"
+      md-confirm-text="Ok"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    requestSuccessful: {
+      type: Boolean,
+      required: true
+    },
+    requestFailed: {
+      type: Boolean,
+      required: true
+    },
+    errorMessage: {
+      type: String,
+      required: true
+    },
+    successMessage: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style></style>
