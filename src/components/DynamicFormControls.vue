fc7fb2c03a7d2768075112c3261089dabaefc82a
David Munkacsi <david.munkacs93@gmail.com>
2020-03-20 16:23:01 +0100

    Make dynamic form controls customizable.

--- src/components/DynamicFormControls.vue
+++ src/components/DynamicFormControls.vue
@@ -1,48 +1,55 @@
 <template>
   <md-list class="control-list">
-    <md-list-item v-for="control in controls" :key="control.order">
-      <md-chips
-        v-if="control.element === CHIPS"
-        v-model="control.value"
-      ></md-chips>
-      <md-datepicker
-        v-model="control.value"
-        v-if="control.element === DATE_PICKER"
-      >
-        <label>{{ control.label }}</label>
-      </md-datepicker>
-      <md-field v-if="control.element === DROP_DOWN">
-        <label>{{ control.label }}</label>
-        <md-select v-model="control.value">
-          <md-option
-            v-for="(value, name, index) in control.values"
-            :value="value"
-            :key="index"
-          >
-            {{ value }}
-          </md-option>
-        </md-select>
-      </md-field>
-      <md-switch v-model="control.value" v-if="control.element === SWITCH">{{
-        control.label
-      }}</md-switch>
-      <md-field v-if="control.element === FLOAT_INPUT">
-        <label>{{ control.label }}</label>
-        <md-input v-model="control.value" type="number" step="0.01"></md-input>
-      </md-field>
-      <md-field v-if="control.element === NUMBER_INPUT">
-        <label>{{ control.label }}</label>
-        <md-input v-model="control.value" type="number"></md-input>
-      </md-field>
-      <md-field v-if="control.element === TEXT_INPUT">
-        <label>{{ control.label }}</label>
-        <md-input v-model="control.value" type="text"></md-input>
-      </md-field>
-    </md-list-item>
+    <draggable v-model="controls">
+      <md-list-item v-for="control in controls" :key="control.order">
+        <md-chips
+          v-if="control.element === CHIPS"
+          v-model="control.value"
+        ></md-chips>
+        <md-datepicker
+          v-model="control.value"
+          v-if="control.element === DATE_PICKER"
+        >
+          <label>{{ control.label }}</label>
+        </md-datepicker>
+        <md-field v-if="control.element === DROP_DOWN">
+          <label>{{ control.label }}</label>
+          <md-select v-model="control.value">
+            <md-option
+              v-for="(value, name, index) in control.values"
+              :value="value"
+              :key="index"
+            >
+              {{ value }}
+            </md-option>
+          </md-select>
+        </md-field>
+        <md-switch v-model="control.value" v-if="control.element === SWITCH">{{
+          control.label
+        }}</md-switch>
+        <md-field v-if="control.element === FLOAT_INPUT">
+          <label>{{ control.label }}</label>
+          <md-input
+            v-model="control.value"
+            type="number"
+            step="0.01"
+          ></md-input>
+        </md-field>
+        <md-field v-if="control.element === NUMBER_INPUT">
+          <label>{{ control.label }}</label>
+          <md-input v-model="control.value" type="number"></md-input>
+        </md-field>
+        <md-field v-if="control.element === TEXT_INPUT">
+          <label>{{ control.label }}</label>
+          <md-input v-model="control.value" type="text"></md-input>
+        </md-field>
+      </md-list-item>
+    </draggable>
   </md-list>
 </template>
 
 <script>
+import draggable from "vuedraggable";
 import {
   DROP_DOWN,
   FLOAT_INPUT,
@@ -55,6 +62,9 @@ import {
 } from "../types/layout-item-types";
 
 export default {
+  components: {
+    draggable
+  },
   props: {
     controls: {
       type: Array,
