09fb7a722861c730f158d933288f7dd189189687
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-26 10:16:11 +0100

    Handle chips control edge case.

--- src/components/DynamicForm.vue
+++ src/components/DynamicForm.vue
@@ -3,20 +3,31 @@
     <md-list>
       <draggable v-model="controls">
         <md-list-item v-for="control in controls" :key="control.order">
-          <md-field :class="getValidationClass(control.label)">
+          <md-field
+            v-if="
+              control.element === DROP_DOWN ||
+                control.element === FLOAT_INPUT ||
+                control.element === NUMBER_INPUT ||
+                control.element === TEXT_INPUT ||
+                control.element === PASSWORD_INPUT
+            "
+            :class="getValidationClass(control.label)"
+          >
             <label :for="control.label">{{ control.label }}</label>
-            <md-chips
-              v-if="control.element === CHIPS"
-              v-model="form[control.label]"
-            ></md-chips>
-            <md-datepicker
-              v-else-if="control.element === DATE_PICKER"
+            <md-select
+              v-if="control.element === DROP_DOWN"
               v-model="form[control.label]"
             >
-            </md-datepicker>
+              <md-option
+                v-for="(value, name, index) in control.values"
+                :value="value"
+                :key="index"
+              >
+                {{ value }}
+              </md-option>
+            </md-select>
             <md-input
               v-else-if="control.element === FLOAT_INPUT"
-              :id="control.label"
               v-model="form[control.label]"
               type="number"
               step="0.01"
@@ -41,24 +52,7 @@
               v-model="form[control.label]"
               type="password"
             ></md-input>
-            <md-select
-              v-else-if="control.element === DROP_DOWN"
-              v-model="form[control.label]"
-            >
-              <md-option
-                v-for="(value, name, index) in control.values"
-                :value="value"
-                :key="index"
-              >
-                {{ value }}
-              </md-option>
-            </md-select>
-            <md-switch
-              v-else-if="control.element === SWITCH"
-              v-model="form[control.label]"
-            >
-              {{ control.label }}
-            </md-switch>
+
             <md-tooltip v-if="control.description" md-direction="left">
               {{ control.description }}
             </md-tooltip>
@@ -83,6 +77,35 @@
               </span>
             </template>
           </md-field>
+          <template
+            v-if="
+              control.element === CHIPS ||
+                control.element === DATE_PICKER ||
+                control.element === SWITCH
+            "
+          >
+            <md-chips
+              v-if="control.element === CHIPS"
+              v-model="form[control.label]"
+            >
+              <label>{{ control.label }}</label>
+            </md-chips>
+
+            <md-switch
+              v-else-if="control.element === SWITCH"
+              v-model="form[control.label]"
+            >
+              <label>{{ control.label }}</label>
+            </md-switch>
+            <md-tooltip v-if="control.description" md-direction="left">
+              {{ control.description }}
+            </md-tooltip>
+            <template v-if="$v.$dirty && $v.form[control.label]">
+              <span class="md-error" v-if="!$v.form[control.label].required">
+                {{ control.label }} is required
+              </span>
+            </template>
+          </template>
         </md-list-item>
       </draggable>
     </md-list>
@@ -129,11 +152,12 @@ export default {
   },
   created() {
     this.controls.forEach(control => {
-      this.$set(this.form, control.label, null);
+      if (control.element === CHIPS) {
+        this.$set(this.form, control.label, []);
+      } else {
+        this.$set(this.form, control.label, null);
+      }
     });
-  },
-  mounted() {
-    console.log(this.$v);
   },
   data: () => ({
     form: {},
@@ -192,7 +216,6 @@ export default {
       if (!this.$v) return;
 
       const field = this.$v.form[fieldName];
-      console.log(field);
       if (field) {
         return {
           "md-invalid": field.$invalid && field.$dirty
