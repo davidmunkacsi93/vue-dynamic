0d93ff73855b53bd2e69cd5e0851e6014c526768
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-26 15:54:59 +0100

    Remove unused procy settings.

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   configureWebpack: config => {
     config.devtool = "source-map";
-  },
-  devServer: {
-    proxy: {
-      "/proxy": {
-        target: "http://localhost:3000",
-        ws: true,
-        changeOrigin: true
-      }
-    }
   }
 };
