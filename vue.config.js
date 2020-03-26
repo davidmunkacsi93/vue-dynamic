7a0df690ca6d27d8c0c8e131a040d65951194137
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-26 13:51:15 +0100

    Add proxy configuration.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   configureWebpack: config => {
     config.devtool = "source-map";
+  },
+  devServer: {
+    proxy: {
+      "/proxy": {
+        target: "http://localhost:3000",
+        ws: true,
+        changeOrigin: true
+      }
+    }
   }
 };
