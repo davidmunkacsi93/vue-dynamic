fa48c991073bbf6d1a3e3b37a9c5a5a2b3788198
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-27 19:49:52 +0100

    Update dependencies.

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   configureWebpack: config => {
     config.devtool = "#inline-source-map";
-    config.vue = {
-      loaders: {
-        js: "babel-loader"
-      }
-    };
   }
 };
