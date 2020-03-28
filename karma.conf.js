743004ef5833c3a7dfcad60c7be204cdf05bbdaf
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-28 16:21:43 +0100

    Configure webpack and babel for karma.

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,12 @@
-// Karma configuration
-// Generated on Sat Mar 28 2020 15:40:48 GMT+0100 (Central European Standard Time)
-
 module.exports = function (config) {
   config.set({
     basePath: "",
 
     plugins: [
+      "karma-webpack",
       "karma-jasmine",
       "karma-babel-preprocessor",
-      "karma-chrome-launcher",
-      "karma-phantomjs-launcher"
+      "karma-chrome-launcher"
     ],
 
     frameworks: ["jasmine"],
@@ -18,51 +15,42 @@ module.exports = function (config) {
 
     exclude: [],
 
-    preprocessors: {
-      "src/**/*.js": ["babel"],
-      "test/**/*.js": ["babel"]
-    },
-    babelPreprocessor: {
-      options: {
-        presets: ["@babel/preset-env"],
-        sourceMap: "inline"
-      },
-      filename: function (file) {
-        return file.originalPath.replace(/\.js$/, ".es5.js");
-      },
-      sourceFileName: function (file) {
-        return file.originalPath;
+    webpack: {
+      module: {
+        rules: [
+          {
+            test: /\.js$/i,
+            exclude: /(node_modules)/,
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ]
       }
     },
+    preprocessors: {
+      "./test/**/*.js": ["webpack"]
+    },
+    webpackMiddleware: {
+      noInfo: true,
+      stats: "errors-only"
+    },
 
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ["progress"],
 
-    // web server port
     port: 9876,
 
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
 
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ["Chrome"],
 
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
 
-    // Concurrency level
-    // how many browser should be started simultaneous
     concurrency: Infinity
   });
 };
