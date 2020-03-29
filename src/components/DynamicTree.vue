446f2f68d4cae4bc22551bce4149fc53c4bdcdf3
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-29 15:38:56 +0200

    Implement simple object and primitive cases in dynamic tree.

--- src/components/DynamicTree.vue
+++ src/components/DynamicTree.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
     <template v-if="Array.isArray(values)"> </template>
-    <template v-else-if="isSimpleObject(values)"></template>
-    <template v-else-if="isPrimitive(values)"></template>
+    <template v-else-if="isNestedObject(values)"> </template>
+    <template v-else-if="isSimpleObject(values)">
+      <dynamic-object-view :values="values"> </dynamic-object-view>
+    </template>
+    <template v-else-if="isPrimitive(values)">
+      <span>{{ values }}</span>
+    </template>
   </div>
 </template>
 
 <script>
 import { isSimpleObject, isPrimitive } from '../utils/object-utils';
 
+import DynamicObjectView from '../components/DynamicObjectView';
+
 export default {
+  components: { DynamicObjectView },
   props: {
     values: {
       required: true
