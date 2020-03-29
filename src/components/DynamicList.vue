0ca12a51b5065314432c281028422d00eb5fef85
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-29 14:03:27 +0200

    Introduce dynamic list for simple results.

---
+++ src/components/DynamicList.vue
@@ -0,0 +1,19 @@
+<template>
+  <md-list>
+    <md-list-item v-for="(value, index) in values" :key="index">
+      <span>{{ value }}</span>
+    </md-list-item>
+  </md-list>
+</template>
+
+<script>
+export default {
+  props: {
+    values: {
+      required: true
+    }
+  }
+};
+</script>
+
+<style></style>
