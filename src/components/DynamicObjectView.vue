4a8db66e96431dccceac388f2996830f59e5eab8
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-29 14:02:59 +0200

    Introduce dynamic object view.

---
+++ src/components/DynamicObjectView.vue
@@ -0,0 +1,36 @@
+<template>
+  <md-list class="md-double-line">
+    <draggable>
+      <md-list-item v-for="header in headers" :key="header">
+        <div class="md-list-item-text">
+          <span>{{ header }}</span>
+          <span v-if="Array.isArray(values)">{{ values[0][header] }}</span>
+          <span v-else>{{ values[header] }}</span>
+        </div>
+      </md-list-item>
+    </draggable>
+  </md-list>
+</template>
+
+<script>
+export default {
+  props: {
+    values: {
+      required: true
+    }
+  },
+  data() {
+    return {
+      headers: []
+    };
+  },
+  watch: {
+    values: function (value) {
+      this.values = value;
+      this.headers = Object.keys(this.values);
+    }
+  }
+};
+</script>
+
+<style></style>
