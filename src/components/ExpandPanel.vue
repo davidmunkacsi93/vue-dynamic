2f0619bb3a0808f0168d4dd322f47294dcf1cc40
David Munkacsi <david.munkacsi93@gmail.com>
2020-03-31 17:44:42 +0200

    Introduce ExpandPanel to reduce code duplication.

---
+++ src/components/ExpandPanel.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div @click.stop="toggleOpen()">
+      <h4 class="display-inline">{{ title }}</h4>
+      <md-icon class="display-inline" v-if="!isOpen">expand_more</md-icon>
+      <md-icon class="display-inline" v-if="isOpen">expand_less</md-icon>
+    </div>
+    <div v-if="isOpen">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      required: false,
+      type: String
+    },
+    isExpanded: {
+      required: true,
+      type: Boolean
+    }
+  },
+  data() {
+    return {
+      open: false
+    };
+  },
+  computed: {
+    isOpen: function () {
+      return this.open || this.currentDepth === 0;
+    }
+  },
+  methods: {
+    toggleOpen() {
+      this.open = !this.open;
+    }
+  }
+};
+</script>
+
+<style>
+.display-inline {
+  display: inline !important;
+}
+</style>
