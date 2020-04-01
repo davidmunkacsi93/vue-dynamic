38dbf52425a4df21a807d13cf569e299c5dffdb6
David Munkacsi <david.munkacsi93@gmail.com>
2020-04-01 11:57:51 +0200

    Add new store module for api keys.

---
+++ src/store/modules/api-keys.js
@@ -0,0 +1,15 @@
+const LOCAL_STORAGE_API_KEYS_KEY = 'api-keys';
+
+const state = {
+  apiKeys: {}
+};
+
+const mutations = {};
+
+const actions = {};
+
+export default {
+  state,
+  mutations,
+  actions
+};
