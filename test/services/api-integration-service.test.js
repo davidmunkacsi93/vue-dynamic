b2ef6f8322dea06aa79b2c0510d263c64c9941dc
David Munkacsi <david.munkacsi93@gmail.com>
2020-04-04 13:45:52 +0200

    Add first unit tests to api-integeration-service.

--- test/services/api-integration-service.test.js
+++ test/services/api-integration-service.test.js
@@ -1,6 +1,6 @@
 import SwaggerParser from 'swagger-parser';
 import OpenApiParser from '../../src/parsers/open-api-parser.js';
-import ApiIntegrationService from '../../src/services/api-integration-service';
+import ApiIntegrationService from '../../src/services/api-integration-service.js';
 
 describe('Tests for api-integration-service integrateNewApi function:', () => {
   it('must throw an error, when swagger validation fails.', async () => {
@@ -9,7 +9,37 @@ describe('Tests for api-integration-service integrateNewApi function:', () => {
     });
 
     ApiIntegrationService.integrateNewAPI('URL').catch((reason) => {
-      expect(reason.message).toBe(validation('Validation failed'));
+      expect(reason.message).toBe('Validation failed');
     });
   });
+
+  it('must throw an error, when swagger parsing fails.', async () => {
+    spyOn(SwaggerParser, 'validate').and.returnValue(
+      Promise.resolve({ swagger: '2.0.' })
+    );
+
+    spyOn(SwaggerParser, 'parse').and.callFake(() => {
+      throw new Error('Parsing failed');
+    });
+
+    ApiIntegrationService.integrateNewAPI('URL').catch((reason) => {
+      expect(reason.message).toBe('Error: Parsing failed');
+    });
+  });
+
+  // it('must throw an error, when OpenAPI parser throws an error.', async () => {
+  //   const swagger = { swagger: '2.0.' };
+  //   spyOn(SwaggerParser, 'validate').and.returnValue(Promise.resolve(swagger));
+
+  //   spyOn(SwaggerParser, 'parse').and.returnValue(Promise.resolve(swagger));
+
+  //   spyOn(OpenApiParser, 'processSpecification').and.callFake(() => {
+  //     throw new Error('Parsing failed');
+  //   });
+
+  //   apiIntegrationService.integrateNewAPI('URL').catch((reason) => {
+  //     console.log(reason);
+  //     expect(reason.message).toBe('Parsing failed');
+  //   });
+  // });
 });
