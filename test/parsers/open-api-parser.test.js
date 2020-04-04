93a4ea616e80b6bc95c8d0cd1e513abc69fe7ead
David Munkacsi <david.munkacsi93@gmail.com>
2020-04-04 17:42:03 +0200

    Add additional unit tests for parser.

--- test/parsers/open-api-parser.test.js
+++ test/parsers/open-api-parser.test.js
@@ -2,6 +2,7 @@ import ApiModelFactory from '../../src/factories/api-model-factory';
 import DynamicComponentFactory from '../../src/factories/dynamic-component-factory';
 import OpenApiParser from '../../src/parsers/open-api-parser.js';
 import OpenApiInformationProvider from '../../src/providers/open-api-information-provider.js';
+import DefaultApiLayoutFactory from '../../src/factories/default-api-layout-factory';
 
 describe('Tests for OpenApiParser processSpecification: ', () => {
   it('throws error, when specification cannot be determined', () => {
@@ -90,12 +91,110 @@ describe('Tests for OpenApiParser processSpecification: ', () => {
     var apiModels = [{ model: {} }];
     spyOn(ApiModelFactory, 'createApiModels').and.returnValue(apiModels);
 
+    spyOn(DynamicComponentFactory, 'createDynamicComponents').and.throwError();
+
     var specification = {
       openApi: '3.0',
-      info: {}
+      info: {},
+      components: {
+        schemas: {}
+      }
     };
     expect(() =>
       OpenApiParser.processSpecification(specification)
     ).toThrowError();
+  });
+
+  it('throws error, when default api layout factory throws error.', () => {
+    var apiInformation = {
+      apiVersion: '3.0',
+      title: 'API',
+      description: 'Description'
+    };
+    spyOn(OpenApiInformationProvider, 'getApiInformation').and.returnValue(
+      apiInformation
+    );
+
+    var serverInformation = {
+      serverURL: 'https://virtserver.com',
+      serverDescription: 'Description'
+    };
+    spyOn(OpenApiInformationProvider, 'getServerInformation').and.returnValue(
+      serverInformation
+    );
+
+    var apiModels = [{ model: {} }];
+    spyOn(ApiModelFactory, 'createApiModels').and.returnValue(apiModels);
+
+    var dynamicComponents = [{ component: {} }];
+    spyOn(DynamicComponentFactory, 'createDynamicComponents').and.returnValue(
+      dynamicComponents
+    );
+
+    spyOn(DefaultApiLayoutFactory, 'getDefaultApiLayout').and.throwError();
+
+    var specification = {
+      openApi: '3.0',
+      info: {},
+      components: {
+        schemas: {}
+      }
+    };
+    expect(() =>
+      OpenApiParser.processSpecification(specification)
+    ).toThrowError();
+  });
+
+  it('throws error, when default api layout factory throws error.', () => {
+    var apiInformation = {
+      apiVersion: '3.0'
+    };
+    spyOn(OpenApiInformationProvider, 'getApiInformation').and.returnValue(
+      apiInformation
+    );
+
+    var serverInformation = {
+      serverURL: 'https://virtserver.com',
+      serverDescription: 'Description'
+    };
+    spyOn(OpenApiInformationProvider, 'getServerInformation').and.returnValue(
+      serverInformation
+    );
+
+    var apiModels = [{ model: {} }];
+    spyOn(ApiModelFactory, 'createApiModels').and.returnValue(apiModels);
+
+    var dynamicComponents = [{ component: {} }];
+    spyOn(DynamicComponentFactory, 'createDynamicComponents').and.returnValue(
+      dynamicComponents
+    );
+
+    var defaultLayout = [
+      { x: 0, y: 0 },
+      { x: 0, y: 0 }
+    ];
+    spyOn(DefaultApiLayoutFactory, 'getDefaultApiLayout').and.returnValue(
+      defaultLayout
+    );
+
+    var specification = {
+      openApi: '3.0',
+      info: {},
+      components: {
+        schemas: {}
+      }
+    };
+    var result = OpenApiParser.processSpecification(specification);
+    expect(result.apiVersion).toBe(apiInformation.apiVersion);
+    expect(result.title).toBe(apiInformation.title);
+    expect(result.description).toBe(apiInformation.description);
+    expect(result.serverURL).toBe(serverInformation.serverURL);
+    expect(result.serverDescription).toBe(serverInformation.serverDescription);
+    expect(result.apiModels).toBe(apiModels);
+    expect(result.dynamicComponents).toBe(dynamicComponents);
+    Object.keys(result.apiLayouts).forEach((screenClass) =>
+      expect(result.apiLayouts[screenClass]).toBe(defaultLayout)
+    );
+    expect(result.compacted).toBe(false);
   });
 });
