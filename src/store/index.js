4a05e1a70c792705dd81f33ec5be002df6d21e1b
David Munkacsi <david.munkacsi93@gmail.com>
2020-04-07 17:12:07 +0200

    Remove store module and unused actions after refactor.

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import apiLayouts from './modules/api-layouts';
 import mainLayout from './modules/main-layout';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    apiLayouts: apiLayouts,
     mainLayout: mainLayout
   }
 });
