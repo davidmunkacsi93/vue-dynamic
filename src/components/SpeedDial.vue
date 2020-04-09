5030542ac3def4fe4027daa389795701a3158e91
David Munkacsi <david.munkacsi93@gmail.com>
2020-04-09 11:07:42 +0200

    Add speed dial for better UX.

---
+++ src/components/SpeedDial.vue
@@ -0,0 +1,13 @@
+<template>
+  <md-speed-dial class="md-fixed md-bottom-right" md-direction="top">
+    <md-speed-dial-target>
+      <md-icon>edit</md-icon>
+    </md-speed-dial-target>
+  </md-speed-dial>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
