f47ece12f6f42cf7a2cd3c9ecfcb6177cf711578
David Munkacsi <david.munkacsi93@gmail.com>
2020-04-30 10:19:30 +0200

    Add constant for timeout.

--- src/services/api-integration-service.js
+++ src/services/api-integration-service.js
@@ -1,23 +1,22 @@
 import SwaggerParser from 'swagger-parser';
 import OpenApiProcessor from '../processors/open-api-processor.js';
 
+const MAXIMUM_TIMEOUT = 10000;
+
 class ApiIntegrationService {
   async integrateNewAPI(url) {
     const timeOut = new Promise((_, reject) =>
-      setTimeout(() => reject(new Error('Timed out')), 10000)
+      setTimeout(() => reject(new Error('Timed out')), MAXIMUM_TIMEOUT)
     );
 
-    return Promise.race(
-      [
-        SwaggerParser.validate(url, {
-          dereference: {
-            circular: false
-          }
-        }),
-        timeOut
-      ],
-      {}
-    )
+    return Promise.race([
+      SwaggerParser.validate(url, {
+        dereference: {
+          circular: false
+        }
+      }),
+      timeOut
+    ])
       .then(async (parsedSpecification) => {
         return OpenApiProcessor.processSpecification(parsedSpecification);
       })
